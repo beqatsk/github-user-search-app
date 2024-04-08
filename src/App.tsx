26060332a8d2fc16277ce4f18f27df716cc4cf6a
beqa <beqa.tskhvediani@gmail.com>
2024-04-08 12:09:43 +0400

    add input styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,11 @@ function App() {
             <HeaderTitleImg></HeaderTitleImg>
           </HeaderTitle>
         </HeaderWrapper>
+        <InputWrapper>
+          <SearchImage></SearchImage>
+          <Input placeholder="Search GitHub username…"></Input>
+          <SearchButton>Search</SearchButton>
+        </InputWrapper>
       </Container>
     </>
   );
@@ -48,6 +53,48 @@ const HeaderTitleImg = styled.div`
   height: 20px;
   background-position: center;
   background-size: cover;
+  cursor: pointer;
+`;
+const InputWrapper = styled.div`
+  position: relative;
+`;
+const SearchImage = styled.div`
+  position: absolute;
+  background-image: url(/images/search.png);
+  width: 20px;
+  height: 20px;
+  left: 10px;
+  top: 40px;
+`;
+const Input = styled.input`
+  width: 100%;
+  height: 60px;
+  font-size: 18px;
+  padding: 6.5px 7px 7.5px 16px;
+  border-radius: 15px;
+  padding: 6.5px 7px 7.5px 35px;
+  border-radius: 15px;
+  margin-top: 20px;
+  &::placeholder {
+    font-size: 13px;
+    font-weight: normal;
+    line-height: 1.92;
+    color: #4b6a9b;
+  }
+`;
+const SearchButton = styled.button`
+  position: absolute;
+  width: 84px;
+  height: 46px;
+  margin: 0 0 0 7px;
+  padding: 12.5px 14px 12.5px 18px;
+  border-radius: 10px;
+  background-color: #0079ff;
+  font-size: 14px;
+  font-weight: bold;
+  color: white;
+  right: 10px;
+  top: 28px;
   cursor: pointer;
 `;
 
